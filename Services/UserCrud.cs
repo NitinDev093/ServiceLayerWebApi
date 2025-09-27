154c9e50fa960ceaec0a8f77d3a2bfebcf7001c9
NitinDev093 <nitinsahani093@gmail.com>
2025-09-27 10:40:22 +0530

    changes

--- Services/UserCrud.cs
+++ Services/UserCrud.cs
@@ -17,20 +17,23 @@ namespace ServiceLayerWebApi.Services
             _configuration = config;
             connectionString = _configuration.GetConnectionString("DefaultConnection");
         }
+
+        [Route("InsertUser")]
+        [HttpPost]
         public ApiResponseModel<string> InsertUser(UserRequestModel requestModel)
         {
             ApiResponseModel<string> response = new ApiResponseModel<string>();
-            using(SqlConnection conn=new SqlConnection(connectionString))
+            using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 SqlCommand cmd = new SqlCommand("usp_insertUsers", conn);
-                cmd.CommandType=CommandType.StoredProcedure;
+                cmd.CommandType = CommandType.StoredProcedure;
                 cmd.Parameters.AddWithValue("@name", requestModel.Name);
                 cmd.Parameters.AddWithValue("@email", requestModel.Email);
                 cmd.Parameters.AddWithValue("@password", requestModel.Password);
                 cmd.Parameters.AddWithValue("@age", requestModel.Age);
                 conn.Open();
-                int result=cmd.ExecuteNonQuery();
-                if (result>0)
+                int result = cmd.ExecuteNonQuery();
+                if (result > 0)
                 {
                     response.IsSuccess = true;
                     response.Message = "User inserted successfully";
@@ -45,18 +48,20 @@ namespace ServiceLayerWebApi.Services
             }
         }
 
+        [Route("GetUsers")]
+        [HttpGet]
         public ApiResponseModel<List<DataBaseResponseModel>> Getusers()
         {
             try
             {
-                ApiResponseModel<List<DataBaseResponseModel>> response= new ApiResponseModel<List<DataBaseResponseModel>>();
+                ApiResponseModel<List<DataBaseResponseModel>> response = new ApiResponseModel<List<DataBaseResponseModel>>();
                 List<DataBaseResponseModel> users = new List<DataBaseResponseModel>();
-                using (SqlConnection sqlcon=new SqlConnection(connectionString))
+                using (SqlConnection sqlcon = new(connectionString))
                 {
                     SqlCommand cmd = new SqlCommand("usp_insertUsersTABLE", sqlcon);
                     cmd.CommandType = CommandType.StoredProcedure;
                     sqlcon.Open();
-                    SqlDataReader rdr=cmd.ExecuteReader();
+                    SqlDataReader rdr = cmd.ExecuteReader();
                     while (rdr.Read())
                     {
                         DataBaseResponseModel user = new DataBaseResponseModel();
@@ -67,20 +72,18 @@ namespace ServiceLayerWebApi.Services
                         user.Age = Convert.ToInt32(rdr["age"]);
                         users.Add(user);
                     }
-                    if (users.Count>0 && users!=null)
+                    if (users.Count > 0 && users != null)
                     {
                         response.IsSuccess = true;
-                        response.Message = "Users retrieved successfully";  
+                        response.Message = "Users retrieved successfully";
                         response.Data = users;
-                        return response;
                     }
                     else
                     {
                         response.IsSuccess = false;
                         response.Message = "No users found";
-                        response.Data = null;
-                        return response;
                     }
+                    return response;
                 }
             }
             catch (Exception ex)
